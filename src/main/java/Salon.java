0c3cd69773076eda30aa081f442d9c99972e034e
ArnoldHenry <arnoldomwoyo2@gmail.com>
2018-10-18 13:58:28 +0300

    created main class

---
+++ src/main/java/Salon.java
@@ -0,0 +1,15 @@
+import static spark.Spark.setPort;
+
+public class Salon {
+    public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
+    }
+}
