5109a95b8d3607955f04e599054a9ffa9c42a4cc
ArnoldHenry <arnoldomwoyo2@gmail.com>
2018-10-20 00:54:55 +0300

    added required validation

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,6 +1,6 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test","arnold","/arnold/");
-    public static Sql2o sql2oHair = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon","arnold","/arnold/");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test","arnold","arnold1234");
+    public static Sql2o sql2oHair = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon","arnold","arnold1234");
 }
